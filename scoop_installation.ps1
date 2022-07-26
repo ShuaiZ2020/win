ba90e241a38bafdac65be68926394b45e9669667
ShuaiZ2020 <2317709047z@gmail.com>
2022-07-25 23:39:09 -0700

    scoop

--- scoop_installation.ps1
+++ scoop_installation.ps1
@@ -1,2 +1,9 @@
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 irm get.scoop.sh | iex
+scoop bucket add extras
+scoop bucket add versions
+scoop install 7zip
+scoop install googlechrome
+scoop install vscode
+scoop install python39
+scoop install potplayer
