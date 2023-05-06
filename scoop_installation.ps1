32af1c07eaccb2f24599e0fe913ed234d20b043f
ShuaiZ2020 <2317709047z@gmail.com>
2023-05-05 23:35:27 -0700

    a

--- scoop_installation.ps1
+++ scoop_installation.ps1
@@ -1,9 +1,7 @@
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 irm get.scoop.sh | iex
+scoop install git
 scoop bucket add extras
 scoop bucket add versions
-scoop install 7zip
-scoop install googlechrome
-scoop install vscode
-scoop install python
-scoop install potplayer
+scoop bucket add my-bucket https://github.com/ShuaiZ2020/my_bucket
+scoop install 7zip googlechrome vscode python39 potplayer my-bucket/clash wechat my-bucket/tim
